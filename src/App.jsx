04c6c8d67bd4e42d446153a373217217eeafd439
rpggamage@gmail.com <rpggamage@gmail.com>
2025-02-15 16:18:34 +0530

    sticky, fix , relative , absolute in tailwind ok

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,20 @@ import './App.css';
 function App() {
     return (
         <div>
-          <div className='w-[500px] h-[500px] bg-blue-200'>
-            <div className='w-[50px] h-[50px] bg-red-200'>  </div>
-            <div className='w-[50px] h-[50px] bg-yellow-200'>  </div>
-            <div className='w-[50px] h-[50px] bg-green-200'>  </div>
+            <div className='w-[800px] h-[800px] bg-yellow-200 relative'>  
+            <div className='w-[500px] h-[500px] bg-blue-200 relative'>
+
+
+
+                <div className='w-[50px] h-[50px] bg-red-500 sticky top-5 left-5'>  </div>            
+                <div className='w-[50px] h-[50px] bg-green-500 absolute right-10 bottom-10'>  </div>
+                <div className='w-[50px] h-[50px] bg-yellow-500 fixed right-3 bottom-3'>  </div>
+
+
+
             </div>
 
-          </div>
+            </div>
         </div>
     );
 }
